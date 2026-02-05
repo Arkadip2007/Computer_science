0ad0083ff5fd58dd880b85d267357817b60e7e7f
Arkadip2007 <arkadipmahapatra2007@gmail.com>
2026-02-05 15:34:50 +0530

    Add shutdown confirmation program in C

---
+++ C/shut.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char choice;
+    printf("Are you sure you want to shutdown the system? (y/n): ");
+    scanf(" %c", &choice);
+
+    if(choice == 'y' || choice == 'Y') {
+        system("shutdown now");
+    } else {
+        printf("Shutdown cancelled.\n");
+    }
+
+    return 0;
+} 
